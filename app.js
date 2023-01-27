7fddc93676ac82f85b4e23446f9da79b41ac74f9
Kati89 <kovacs.katalin@sapientia.ms.ro>
2023-01-27 01:04:56 -1000

    AddShoes

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 
-
+let btnAdd=document.querySelector(".add");
+let inp1=document.getElementById('model');
+let inp2=document.getElementById('material');
+let inp3=document.getElementById('culoare');
+let inp4=document.getElementById('pret');
 
 let listContainer=document.querySelector(".container");
 
@@ -20,4 +24,20 @@ model.addEventListener("change",(e)=>{
         
     })
 })
-//
+
+
+btnAdd.addEventListener("click",()=>{
+    let pantof={model:inp1.value,material:inp2.value,culoare:inp3.value,pret:+inp4.value};
+    addShoes(pantof).then(data=>{
+        let container=document.querySelector(".container");
+        let s=createRow(data);
+
+        container.appendChild(s);
+    })
+
+})
+
+let containerSlect=document.querySelector(".modele");
+getAllModels().then(data=>{
+    creteOptions(data);
+})
